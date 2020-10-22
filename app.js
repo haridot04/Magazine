764e2ffa42249f6eba99b4f041cd935234a8b7bf
Harikrishnan R U <hari@Harikrishnans-MacBook-Pro.local>
2020-10-22 18:44:23 +0530

    fliphtml

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ tl.fromTo('#title', { opacity: 0 }, { opacity: 1, duration: 2 });
 tl.fromTo('#college', { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
 tl.fromTo('#read', { opacity: 0 }, { opacity: 1, duration: 1 }, "-=0.2");
+tl.fromTo('#read', { opacity: 0 }, { opacity: 1, duration: 1 }, "-=0.2");
+
 
 var img = document.querySelector('main');
 var freewayEaseTween = new TimelineMax({
@@ -15,13 +17,3 @@ var freewayEaseTween = new TimelineMax({
     yoyo: true,
 });
 
-freewayEaseTween
-    .set(img, { backgroundSize: "100% 100%" })
-    // animate CSS autoAlpha to 1
-    .to(img, 5, {
-        backgroundSize: "+=100% +=50%",
-        autoRound: false,
-        ease: Power1.ease0ut
-    })
-    .progress(1).progress(0)
-    .play(); 
